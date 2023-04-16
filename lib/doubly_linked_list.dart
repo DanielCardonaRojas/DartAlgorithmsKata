5cfdd3a8f24e309ec7b757fc1ba2a93816437d99
Daniel Cardona Rojas <d.cardona.rojas@gmail.com>
2023-04-16 08:22:34 -0500

    improve doubly linked list tests

--- lib/doubly_linked_list.dart
+++ lib/doubly_linked_list.dart
@@ -1,5 +1,6 @@
 class DoublyLinkedList<T> {
-  DoublyLinkedListNode? root;
+  DoublyLinkedListNode<T>? root;
+  DoublyLinkedListNode<T>? tail;
   int _length = 0;
 
   int get length => _length;
@@ -9,6 +10,17 @@ class DoublyLinkedList<T> {
   void prepend(T value) {}
 
   void delete(int index) {}
+  void insert(T value, {required int at}) {
+  List<T> collect() {
+    var current = root;
+    var result = <T>[];
+    while (current != null) {
+      result.add(current.value);
+      current = current.next;
+    }
+
+    return result;
+  }
 
   T? itemAt(int index) {
     return null;
@@ -17,8 +29,8 @@ class DoublyLinkedList<T> {
 
 class DoublyLinkedListNode<T> {
   T value;
-  DoublyLinkedListNode? next;
-  DoublyLinkedListNode? previous;
+  DoublyLinkedListNode<T>? next;
+  DoublyLinkedListNode<T>? previous;
 
   DoublyLinkedListNode({required this.value});
 }
--- test/doubly_linked_list_test.dart
+++ test/doubly_linked_list_test.dart
@@ -6,12 +6,14 @@ void main() {
     final ll = DoublyLinkedList<int>();
     ll.prepend(8);
     assert(ll.length == 1);
+    assert(ll.root?.value == 8);
   });
 
-  test('prepend increases size', () {
+  test('append increases size', () {
     final ll = DoublyLinkedList<int>();
     ll.append(1);
     assert(ll.length == 1);
+    assert(ll.tail?.value == 1);
   });
 
   test('delete decreases size', () {
@@ -21,13 +23,35 @@ void main() {
     assert(ll.length == 0);
   });
 
+  test('list with single item makes tail and head match', () {
+    final ll = DoublyLinkedList();
+    ll.append(1);
+    assert(ll.root == ll.tail);
+  });
+
+  test('insert at a given position', () {
+    final ll = DoublyLinkedList();
+    ll.append(1);
+    ll.append(2);
+    ll.append(4);
+    ll.insert(3, at: 2);
+
+    assert(ll.itemAt(2) == 3);
+    assert(ll.itemAt(3) == 4);
+    assert(ll.length == 4);
+  });
+
   test('can read value at a particular index', () {
     final ll = DoublyLinkedList<int>();
     ll.append(3);
     ll.append(7);
     ll.append(8);
 
+    assert(ll.itemAt(2) == 8);
     assert(ll.itemAt(1) == 7);
+    assert(ll.itemAt(0) == 3);
+    assert(ll.tail?.value == 8);
+    assert(ll.root?.value == 3);
     assert(ll.length == 3);
   });
 }
