86007df662232deea12f2b632384a126f0f2d3eb
Daniel Cardona Rojas <d.cardona.rojas@gmail.com>
2023-04-16 07:14:18 -0500

    linked list implementation

--- lib/linked_list.dart
+++ lib/linked_list.dart
@@ -4,22 +4,72 @@ class LinkedList<T> {
 
   int get length => _length;
 
-  void append(T value) {}
+  void append(T value) {
+    _length += 1;
+    if (root == null) {
+      root = LinkedListNode(value: value);
+      return;
+    }
 
-  void prepend(T value) {}
+    var current = root;
+    while (current?.next != null) {
+      current = current?.next;
+    }
 
-  void delete(int index) {}
+    current?.next = LinkedListNode(value: value);
+  }
+
+  void prepend(T value) {
+    _length += 1;
+    if (root == null) {
+      root = LinkedListNode(value: value);
+      return;
+    }
+
+    final next = root;
+
+    root = LinkedListNode(value: value);
+    root?.next = next;
+  }
+
+  void delete(int index) {
+    if (index >= length) return;
+    _length -= 1;
+    final previous = _nodeAt(index - 1);
+    final next = previous?.next?.next;
+    previous?.next = next;
+  }
+
   void insert(T value, int index) {
+    if (index >= length) return;
+    _length += 1;
+    final previous = _nodeAt(index - 1);
+    final current = previous?.next;
+    final newNode = LinkedListNode(value: value);
+    previous?.next = newNode;
+    newNode.next = current;
+  }
+
+  LinkedListNode<T>? _nodeAt(int index) {
+    if (index >= length) return null;
+
+    var current = root;
+
+    for (var i = 0; i < index; i++) {
+      current = current?.next;
+    }
+
+    return current;
   }
 
   T? itemAt(int index) {
-    return null;
+    return _nodeAt(index)?.value;
   }
 }
 
 class LinkedListNode<T> {
   T value;
-  LinkedListNode? next;
+  LinkedListNode<T>? next;
 
   LinkedListNode({required this.value});
 }
