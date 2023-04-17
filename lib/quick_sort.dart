957f219bb19e55736496028c77164ef32cc87529
Daniel Cardona Rojas <daniel.cardona@ymedialabs.com>
2023-04-17 14:40:35 -0500

    quick sort implementation

--- lib/quick_sort.dart
+++ lib/quick_sort.dart
@@ -1,1 +1,28 @@
-void quickSort(List<int> array) {}
+void quickSort(List<int> array) {
+  _quickSort(array, 0, array.length - 1);
+}
+
+void _quickSort(List<int> array, int start, int end) {
+  if (end <= start) {
+    return;
+  }
+  var pivotIndex = splitSort(array, start, end);
+  _quickSort(array, start, pivotIndex - 1);
+  _quickSort(array, pivotIndex, end);
+}
+
+int splitSort(List<int> array, int start, int pivotIndex) {
+  final pivot = array[pivotIndex];
+
+  for (var i = start; i <= pivotIndex; i++) {
+    final value = array[i];
+    if (value >= pivot) {
+      //swap
+      array[pivotIndex] = value;
+      array[i] = pivot;
+      pivotIndex = i;
+    }
+  }
+
+  return pivotIndex;
+}
