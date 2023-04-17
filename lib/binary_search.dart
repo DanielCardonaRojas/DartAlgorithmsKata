77833e70297d011fbd1aabd7891392997012733a
Daniel Cardona Rojas <daniel.cardona@ymedialabs.com>
2023-04-17 08:23:47 -0500

    binary search solutions

--- lib/binary_search.dart
+++ lib/binary_search.dart
@@ -1,3 +1,20 @@
 int? binarySearch(List<int> haystack, int needle) {
+  int startIndex = 0;
+  int endIndex = haystack.length - 1;
+
+  while (startIndex < endIndex) {
+    final midIndex = (startIndex + endIndex) ~/ 2;
+
+    final value = haystack[midIndex];
+
+    if (needle == value) {
+      return midIndex;
+    } else if (needle < value) {
+      endIndex = midIndex;
+    } else {
+      startIndex = midIndex + 1;
+    }
+  }
+
   return null;
 }
