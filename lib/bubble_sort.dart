fc88ad00aa502567242ab3dc33dfb075316048b7
Daniel Cardona Rojas <daniel.cardona@ymedialabs.com>
2023-04-17 08:24:45 -0500

    bubble sort solutions

--- lib/bubble_sort.dart
+++ lib/bubble_sort.dart
@@ -1,1 +1,14 @@
-void bubbleSort(List<int> array) {}
+void bubbleSort(List<int> array) {
+  var length = array.length;
+
+  while (length > 1) {
+    for (var i = 0; i < length - 1; i++) {
+      if (array[i + 1] < array[i]) {
+        final temp = array[i];
+        array[i] = array[i + 1];
+        array[i + 1] = temp;
+      }
+    }
+    length = length - 1;
+  }
+}
